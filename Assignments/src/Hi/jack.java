f327c8aa885eb93c5187bd90a5a6e127eed9a8c7
PEDAMALA <pvssai135@gmail.com>
2022-12-12 00:02:04 +0530

    threee

---
+++ Assignments/src/Hi/jack.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Hi;
+
+/**
+ * @author dell
+ *
+ */
+public class jack {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
